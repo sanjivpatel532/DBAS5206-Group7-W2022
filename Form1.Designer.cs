c721a4db6a1faf3d3627c5a04f141b5be712a16f
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-04-04 12:12:03 -0400

    Updated changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,24 @@ namespace LakeridgeCommunityHospital
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(779, 438);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(200, 100);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1835, 1007);
+			this.Controls.Add(this.groupBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -43,6 +54,8 @@ namespace LakeridgeCommunityHospital
 		}
 
 		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
 	}
 }
 
