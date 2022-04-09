23d23946eb85ddae064d4dc2ec08427e6e460da8
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-04-09 15:12:05 -0400

    Added Database Connection

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace LakeridgeCommunityHospital.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KUROMATSU-MSI\\SQLEXPRESS;Initial Catalog=LakeRidgeHospital;Integrated" +
+            " Security=True")]
+        public string LakeRidgeHospitalConnectionString {
+            get {
+                return ((string)(this["LakeRidgeHospitalConnectionString"]));
+            }
+        }
     }
 }
